9ecd59e2832e7145b3832667040e048c8dfbec6e
Sage Choi <coolcuds@gmail.com>
2023-04-21 21:31:20 -0700

    Added jsonify to all return statements.

--- app/routes.py
+++ app/routes.py
@@ -1,21 +1,22 @@
-from flask import Blueprint, jsonify,
+from flask import Blueprint, jsonify
 
 class Planet:
-    def __init__(self, id, planet_name, num_of_moons, description):
+    def __init__(self, id, planet_name, description, num_of_moons):
         self.id = id
         self.planet_name = planet_name
-        self.num_of_moons = num_of_moons
         self.description = description
+        self.num_of_moons = num_of_moons
+
 
 planets_list = [
-    Planet(1, "Mercury", 0, "A grey, rocky planet"),
-    Planet(2, "Venus", 0, "A yellow-white, marble planet"),
-    Planet(3, "Earth", 1, "A blue marble with different colored swirls"),
-    Planet(4, "Mars", 2, "A reddish, dusty, cold desert"),
-    Planet(5, "Jupiter", 90, "Covered in swirling cloud stripes"),
-    Planet(6, "Saturn", 83, "Unique planet with rings of ice"),
-    Planet(7, "Uranus", 27, "A baby blue ice giant"),
-    Planet(8, "Neptune", 14, "A dark blue planet with supersonic winds")
+    Planet(1, "Mercury", "A grey, rocky planet", 0),
+    Planet(2, "Venus", "A yellow-white, marble planet", 0),
+    Planet(3, "Earth", "A blue marble with different colored swirls", 1),
+    Planet(4, "Mars", "A reddish, dusty, cold desert", 2),
+    Planet(5, "Jupiter", "Covered in swirling cloud stripes", 90),
+    Planet(6, "Saturn", "Unique planet with rings of ice", 83),
+    Planet(7, "Uranus", "A baby blue ice giant", 27),
+    Planet(8, "Neptune", "A dark blue planet with supersonic winds", 14)
 ]
 
 planets_bp = Blueprint("planet", __name__, url_prefix="/planets")
@@ -27,9 +28,10 @@ def get_all_planets():
         response.append({
             "id": planet.id,
             "planet_name": planet.planet_name,
-            "number_of_moons": planet.num_of_moons,
-            "description": planet.description
+            "description": planet.description,
+            "number_of_moons": planet.num_of_moons
         })
+
     return jsonify(response), 200
 
 @planets_bp.route("/<planet_id>", methods=["GET"])
@@ -37,15 +39,15 @@ def get_one_planet(planet_id):
     try:
         planet_id = int(planet_id)
     except ValueError:
-        return {"message:": f"ID '{planet_id}' is invalid."}, 400
+        return jsonify({"message:": f"ID '{planet_id}' is invalid."}), 400
 
     for planet in planets_list:
         if planet.id == planet_id:
             return {
                 "id": planet.id,
                 "planet_name": planet.planet_name,
-                "number_of_moons": planet.num_of_moons,
-                "description": planet.description
+                "description": planet.description,
+                "number_of_moons": planet.num_of_moons
             }, 200
 
-    return {"message": f"Planet '{planet_id}' does not exist."}, 404
+    return jsonify({"message": f"Planet '{planet_id}' does not exist."}), 404
