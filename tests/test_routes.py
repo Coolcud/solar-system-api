f56d8b381710f647a54cb55d47b28ba5c29714af
Sage Choi <coolcuds@gmail.com>
2023-05-03 15:17:18 -0700

    Created tests for getting 1 planet with no data, some data, and creating a planet.

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,5 +1,25 @@
 import pytest
 
+def test_get_one_planet_with_no_data(client):
+    response = client.get("/planets/1")
+
+    response_body = response.get_json()
+
+    assert response.status_code == 404
+
+def test_get_one_planet(client, two_planets):
+    response = client.get("/planets/1")
+
+    response_body = response.get_json()
+
+    assert response.status_code == 200
+    assert response_body == {
+        "id": 1,
+        "planet_name": "Uranus",
+        "description": "A baby blue ice giant",
+        "num_moons": 27
+    }
+
 def test_get_all_planets(client, two_planets):
     response = client.get("/planets")
 
@@ -18,3 +38,15 @@ def test_get_all_planets(client, two_planets):
         "description": "A reddish, dusty, cold desert",
         "num_moons": 2
     }]
+
+def test_post_creates_planet(client):
+    response = client.post("/planets", json= {
+        "planet_name": "Uranus",
+        "description": "A baby blue ice giant",
+        "num_moons": 27
+    })
+
+    response_body = response.get_json()
+
+    assert response.status_code == 201
+    assert response_body == "Planet 'Uranus' successfully created!"
