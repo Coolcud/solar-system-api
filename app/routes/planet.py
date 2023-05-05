4db442f7d7394c931ddb2ed6f39b13b28018f387
Sarah Kim <kimsuyon2013@gmail.com>
2023-05-05 11:20:39 -0700

    refactored validate planet added from_dict to POST route

--- app/routes/planet.py
+++ app/routes/planet.py
@@ -7,11 +7,7 @@ planets_bp = Blueprint("planet", __name__, url_prefix="/planets")
 @planets_bp.route("", methods=["POST"])
 def create_planet():
     request_body = request.get_json()
-    new_planet = Planet(
-        planet_name = request_body["planet_name"],
-        description = request_body["description"],
-        num_moons = request_body["num_moons"]
-    )
+    new_planet = Planet.from_dict(request_body)
 
     db.session.add(new_planet)
     db.session.commit()
@@ -37,20 +33,20 @@ def get_all_planets():
 
 @planets_bp.route("/<planet_id>", methods=["GET"])
 def get_one_planet(planet_id):
-    planet = validate_planet(planet_id)
+    planet = validate_celestial_body(Planet, planet_id)
     return planet.to_dict(), 200
 
-def validate_planet(planet_id):
+def validate_celestial_body(model, celestial_id):
     try:
-        planet_id = int(planet_id)
+        celestial_id = int(celestial_id)
     except:
-        return abort(make_response({"message:": f"Planet ID '{planet_id}' is invalid."}, 400))
+        return abort(make_response({"message:": f"ID '{celestial_id}' is invalid."}, 400))
 
-    return Planet.query.get_or_404(planet_id, f"Planet ID '{planet_id}' not found.")
+    return model.query.get_or_404(celestial_id, f"ID '{celestial_id}' not found.")
 
 @planets_bp.route("/<planet_id>", methods=["PUT"])
 def update_planet(planet_id):
-    planet = validate_planet(planet_id)
+    planet = validate_celestial_body(Planet, planet_id)
 
     request_body = request.get_json()
     planet.name = request_body["planet_name"]
@@ -63,7 +59,7 @@ def update_planet(planet_id):
 
 @planets_bp.route("/<planet_id>", methods=["DELETE"])
 def delete_planet(planet_id):
-    planet = validate_planet(planet_id)
+    planet = validate_celestial_body(Planet, planet_id)
 
     db.session.delete(planet)
     db.session.commit()
