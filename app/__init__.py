445c2ec2b3449c90c8e008a0524a749a8ad0d76e
Sage Choi <coolcuds@gmail.com>
2023-05-09 11:31:52 -0700

    Connected Render database

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from dotenv import load_dotenv
-import os 
+import os
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -10,11 +10,11 @@ load_dotenv()
 
 def create_app(test_config=None):
     app = Flask(__name__)
-    
+
     if not test_config:
         app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
-    else: 
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("RENDER_DATABASE_URI")
+    else:
         app.config["TESTING"] = True
         app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
         app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_TEST_DATABASE_URI")
